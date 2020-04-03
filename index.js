0a04d58ef5984137a56b3c52c1edc3241a1ebd5e
Marcelino Eilert <marcelinoeilert@MacBook-Air-de-Marcelino.local>
2020-04-03 06:11:01 -0400

    feature: queryparams 2

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const express = require('express');
 const server = express()
 
 server.get('/teste', (req, res) => {
-  return res.json({ hello: 'world2' })
+  const nome = req.query.nome
+
+  
+  return res.json({ hello: `Olá ${nome} ` })
 })
 
 server.listen(3000);
