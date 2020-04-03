4b41701ed262c04f8898f5b3fd7193958f986f70
Marcelino Eilert <marcelinoeilert@MacBook-Air-de-Marcelino.local>
2020-04-03 07:29:49 -0400

    feature: routeparams

--- index.js
+++ index.js
@@ -2,11 +2,17 @@ const express = require('express');
 
 const server = express()
 
+//Query params
+
 server.get('/teste', (req, res) => {
   const nome = req.query.nome
-
-  
   return res.json({ hello: `Olá ${nome} ` })
+})
+
+//Route params 
+server.get('/users/:id', (req,res) => {
+  const id = req.params.id
+  return res.json({ hello: `Buscando o usuario ${id} ` })
 })
 
 server.listen(3000);
