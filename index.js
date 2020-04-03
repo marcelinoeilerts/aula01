e914f6799e706ec00c25268484e0e687da9984a5
Marcelino Eilert <marcelinoeilert@MacBook-Air-de-Marcelino.local>
2020-04-03 09:50:03 -0400

    feature: request-body

--- index.js
+++ index.js
@@ -2,19 +2,27 @@ const express = require('express');
 
 const server = express()
 
-//Query params
+//Query params => http://localhost:3000/teste?Marcelino
 
 server.get('/teste', (req, res) => {
   const nome = req.query.nome
   return res.json({ hello: `Olá ${nome} ` })
 })
 
-//Route params 
+//Route params => http://localhost:3000/1
 server.get('/users/:id', (req,res) => {
   
   //desconstrucao const id = req.params.id
   const { id } = req.params
   return res.json({ hello: `Buscando o usuario ${id} ` })
+})
+
+//Request body
+const users = [ 'Marcelino', 'Carlos', 'Camila' ]
+server.get('/user/:index', (req, res) => {
+  const { index } = req.params
+
+  return res.json(users[index])
 })
 
 server.listen(3000);
